6e3ef918a16f7e3f39f9b897572b8d8a2fff6a4e
Łukasz Pazdro <57200373+LuKaN1u@users.noreply.github.com>
2021-06-27 15:25:10 -0700

    removing vowels

---
+++ codeWars.py
@@ -0,0 +1,6 @@
+def disemvowel(string):
+    return print("-".join(c for c in string if c.lower() not in "aeiou"))
+    # join means what character will be replaced
+    # it means search each element in string for the elements that aren't vowels and replace them
+
+disemvowel("Hello everybody my name is lukas")
