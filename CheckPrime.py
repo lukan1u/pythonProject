e71fb7b5638d17687532a2d901f4515155ad0116
Łukasz Pazdro <57200373+LuKaN1u@users.noreply.github.com>
2021-06-27 15:13:15 -0700

    finished prime task

---
+++ CheckPrime.py
@@ -0,0 +1,19 @@
+from os import system
+import time
+
+def clear():
+    return system("clear")
+
+
+def prime_checker(x):
+    if x % 2 == 0:
+        print("➔➔ your number is dividable by 2")
+    elif x % 2 != 0:
+        print("➔➔ your number is not a prime number")
+
+    time.sleep(3)
+    clear()
+
+prime_checker(int(input("Enter your number to check wheather it's prime\n>>> ")))
+
+
