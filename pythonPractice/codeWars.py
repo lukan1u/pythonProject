30206d1b7cdeec45aa517702118c299c74284e11
Lukas Pazdro <lukaszbiznes21@gmail.com>
2021-11-23 20:20:36 +0000

    Remove -

--- pythonPractice/codeWars.py
+++ pythonPractice/codeWars.py
@@ -1,5 +1,5 @@
 def disemvowel(string):
-    return print("-".join(c for c in string if c.lower() not in "aeiou"))
+    return print("".join(c for c in string if c.lower() not in "aeiou"))
     # join means what character will be replaced
     # it means search each element in string for the elements that aren't vowels and replace them
 
