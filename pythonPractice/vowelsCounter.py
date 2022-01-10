7e8e639b1fff6084fc1f93edc0335ce6a38b476b
lukas <lukaszbiznes21@gmail.com>
2022-01-10 21:03:14 +0000

    task finished

---
+++ pythonPractice/vowelsCounter.py
@@ -0,0 +1,6 @@
+def get_count(sentence):
+    vowels = ["a","e", "i", "o", "u"]
+    final = [elements for elements in sentence if elements in vowels]
+    return print(len(final))
+
+get_count("aeiou")
