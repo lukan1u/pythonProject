fcf3d539754a52891a69f420c7fbae62c241164b
lukas <lukaszbiznes21@gmail.com>
2022-01-11 22:30:20 +0000

    substracting lists

--- pythonPractice/substractingArreys.py
+++ pythonPractice/substractingArreys.py
@@ -1,11 +1,9 @@
 def arrey_diff(a, b):
     listo = []
-    for element1 in a:
-        for element2 in b:
-            listo.append(element1 - element2)
-        return print(listo)
+    for element1, element2 in zip(a,b):
+        listo.append(element1 - element2)
+    return print(listo)
 
-
-listo1 = [300, 100]
-listo2 = [9,8,7,5,5,4,3,2,1]
-arrey_diff(listo1, listo1)
+listo1 = [1,2,3,4,5,6,7,8,9]
+listo2 = [5,4,3,2,1,2,3,4]
+arrey_diff(listo1, listo2)
